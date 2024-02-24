1faba8a7678b9bbdef51c33fa5d676b96e382783
silva4dev <web4dev.lucas@outlook.com>
2024-02-24 20:43:01 -0300

    feat: add tasks route

---
+++ src/root/routes/grape/tasks_routes.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'grape'
+require_relative '../../../http/controllers/tasks_controller'
+require_relative '../../factories/controllers/tasks_controller_factory'
+
+class TasksRoute < Grape::API
+  post :tasks do
+    http_response = TasksControllerFactory.create(
+      TasksController.new, :find
+    ).call(request)
+    status http_response[:status_code]
+    present http_response[:body]
+  end
+end
