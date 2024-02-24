96d1fb121fa93210d495f7ffb6434a1821af7de4
silva4dev <web4dev.lucas@outlook.com>
2024-02-24 03:21:05 -0300

    feat: add find by id task use case

---
+++ src/data/usecases/db_find_by_id_task.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../../core/usecases/find_by_id_task'
+
+class DbFindByIdTask
+  include FindByIdTask
+
+  def initialize(task_repository)
+    @task_repository = task_repository
+  end
+
+  def execute(id)
+    task = @task_repository.find_by_id(id)
+    raise StandardError("Task with id #{id} not found") if task.nil?
+
+    task
+  end
+end
