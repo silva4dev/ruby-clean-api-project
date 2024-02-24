10634d15d18a80229056618b611736abfbb1a391
silva4dev <web4dev.lucas@outlook.com>
2024-02-24 03:21:15 -0300

    feat: add fink tasks use case

---
+++ src/data/usecases/db_find_tasks.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../../core/usecases/find_tasks'
+
+class DbFindTasks
+  include FindTasks
+
+  def initialize(task_repository)
+    @task_repository = task_repository
+  end
+
+  def execute
+    @task_repository.find
+  end
+end
