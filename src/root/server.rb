1fb90e47bf7dcda4da0a352d87351b38a2835076
silva4dev <web4dev.lucas@outlook.com>
2024-02-24 20:42:28 -0300

    feat: ensure add tasks routes

--- src/root/server.rb
+++ src/root/server.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 require 'grape'
+require_relative 'routes/grape/tasks_routes'
 
 class Server < Grape::API
   prefix :api
   format :json
 
-  get :hello do
-    { message: 'Hello, world!' }
-  end
+  mount TasksRoute
 end
 
 Rack::Server.start(app: Server, Port: 3333) if $PROGRAM_NAME == __FILE__
