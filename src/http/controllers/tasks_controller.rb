aa2a9e5bb2d1d87dfa43d4dfce8c9cd69b7e09d4
silva4dev <web4dev.lucas@outlook.com>
2024-02-25 01:41:52 -0300

    refactor: ensure remove methods unnecessaries

--- src/http/controllers/tasks_controller.rb
+++ src/http/controllers/tasks_controller.rb
@@ -6,9 +6,4 @@ class TasksController
   def find(http_request)
     HttpHelper.ok({ message: 'Hello World' })
   end
-
-  def add(http_request); end
-  def find_by_id(http_request); end
-  def update(http_request); end
-  def destroy(http_request); end
 end
