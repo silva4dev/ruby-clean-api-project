345347278bd8a1f9398fe55384da0212b5d4a051
silva4dev <web4dev.lucas@outlook.com>
2024-02-25 20:56:09 -0300

    feat: add find tasks controller

---
+++ src/http/controllers/find_tasks/find_tasks_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../../helpers/http_helper'
+require_relative '../../contracts/controller'
+
+class FindTasksController
+  include Controller
+
+  # @param find_tasks [FindTasks]
+  def initialize(find_tasks_usecase)
+    @find_tasks_usecase = find_tasks_usecase
+  end
+
+  def handle(http_request)
+    tasks = @find_tasks_usecase.execute
+    HttpHelper.ok({ tasks: })
+  end
+end
--- src/http/controllers/tasks_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative '../helpers/http_helper'
-
-class TasksController
-  def find(http_request)
-    HttpHelper.ok({ message: 'Hello World' })
-  end
-end
