e01414f13dbb61af7642b47f57dfb10517473ea3
silva4dev <web4dev.lucas@outlook.com>
2024-02-25 01:44:42 -0300

    feat: add db config

---
+++ src/root/config/db.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../config/env'
+require_relative '../../external/db/postgresql/helpers/postgresql_helper'
+
+PostgreSQLHelper.instance.connect(
+  dbname: Env::POSTGRESQL[:DBNAME],
+  user: Env::POSTGRESQL[:USER],
+  password: Env::POSTGRESQL[:PASSWORD],
+  host: Env::POSTGRESQL[:HOST],
+  port: Env::POSTGRESQL[:PORT]
+)
