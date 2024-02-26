10dea1bc1dcf218e66aa5c08b05de9668d3a45e5
silva4dev <web4dev.lucas@outlook.com>
2024-02-25 22:35:54 -0300

    feat: add invalid type error

---
+++ src/core/errors/invalid_type_error.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class InvalidTypeError < StandardError
+  def initialize(attribute, expected_type)
+    super("#{attribute.capitalize} must be of type #{expected_type}")
+  end
+end
