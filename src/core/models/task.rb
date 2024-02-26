3074a33773c02ae71c926841fbfed70b4023b17c
silva4dev <web4dev.lucas@outlook.com>
2024-02-25 22:37:15 -0300

    feat: add validate in task model

--- src/core/models/task.rb
+++ src/core/models/task.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'securerandom'
+require_relative '../errors/invalid_type_error'
+require_relative '../errors/missing_value_error'
 
 class Task
   # @return [String]
@@ -19,10 +21,20 @@ class Task
     @title = title
     @description = description
     @completed = false
+    validate
   end
 
   # @return [void]
   def mark_as_completed
     @completed = true
+  end
+
+  private
+
+  def validate
+    raise InvalidTypeError.new('Title', 'String') unless title.is_a?(String)
+    raise InvalidTypeError.new('Description', 'String') unless description.is_a?(String)
+    raise MissingValueError, 'Title' if title.empty?
+    raise MissingValueError, 'Description' if description.empty?
   end
 end
