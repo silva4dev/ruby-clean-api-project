a1950a3b1bb8c6e8f195de8b32c547903adc35b8
silva4dev <web4dev.lucas@outlook.com>
2024-03-09 17:13:51 -0300

    feat: add server error helper

--- src/http/helpers/http_helper.rb
+++ src/http/helpers/http_helper.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 require_relative '../contracts/http'
+require_relative '../errors/server_error'
 
 class HttpHelper
   include HttpResponse
 
   def self.ok(data)
     { status_code: 200, body: data }
+  end
+
+  def self.server_error
+    { status_code: 500, body: ServerError.new }
   end
 end
