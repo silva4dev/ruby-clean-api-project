68082e7a96b8b5fc56e79c87a0310f026f732514
silva4dev <web4dev.lucas@outlook.com>
2024-03-09 18:21:07 -0300

    chore: ensure rename pattern

--- tests/spec_helper.rb
+++ tests/spec_helper.rb
@@ -11,6 +11,6 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
-  config.pattern = 'tests/**/*_{test,spec}.rb'
+  config.pattern = 'tests/**/*_spec.rb'
   config.include Rack::Test::Methods
 end
