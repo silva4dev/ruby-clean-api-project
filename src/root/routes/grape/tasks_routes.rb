705bedcb201dc5be3952710ec06dcf59fe590563
silva4dev <web4dev.lucas@outlook.com>
2024-03-10 01:15:52 -0300

    feat: add find by task route

--- src/root/routes/grape/tasks_routes.rb
+++ src/root/routes/grape/tasks_routes.rb
@@ -3,11 +3,20 @@
 require 'grape'
 require_relative '../../adapters/route_adapter'
 require_relative '../../factories/controllers/find_tasks/find_tasks_controller_factory'
+require_relative '../../factories/controllers/find_by_id_task/find_by_id_task_controller_factory'
 
 class TasksRoute < Grape::API
-  get :tasks do
-    http_response = RouteAdapter.adapt(FindTasksControllerFactory.create).call(request)
-    status http_response[:status_code]
-    present http_response[:body]
+  namespace :tasks do
+    get '/' do
+      http_response = RouteAdapter.adapt(FindTasksControllerFactory.create).call(request)
+      status http_response[:status_code]
+      present http_response[:body]
+    end
+
+    get '/:id' do
+      http_response = RouteAdapter.adapt(FindByIdTaskControllerFactory.create).call(request)
+      status http_response[:status_code]
+      present http_response[:body]
+    end
   end
 end
