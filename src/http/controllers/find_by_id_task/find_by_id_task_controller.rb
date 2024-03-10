0a617c03e180a92dd3cfda15339ecf1ee2503aa6
silva4dev <web4dev.lucas@outlook.com>
2024-03-10 01:11:58 -0300

    feat: add find by id task controller

---
+++ src/http/controllers/find_by_id_task/find_by_id_task_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../../helpers/http_helper'
+require_relative '../../contracts/controller'
+
+class FindByIdTaskController
+  include Controller
+
+  # @param find_tasks [FindByIdTask]
+  def initialize(find_by_id_task_usecase)
+    @find_by_id_task_usecase = find_by_id_task_usecase
+  end
+
+  def handle(http_request)
+    task = @find_by_id_task_usecase.execute(http_request[:params][:id])
+    HttpHelper.ok(
+      {
+        id: task[:id],
+        title: task[:title],
+        description: task[:description],
+        completed: task[:completed]
+      }
+    )
+  rescue StandardError
+    HttpHelper.server_error
+  end
+end
