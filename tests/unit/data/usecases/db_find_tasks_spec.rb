ac1771e7abfe609606d1b9e51b56f5a2c696b83e
silva4dev <web4dev.lucas@outlook.com>
2024-03-09 22:04:01 -0300

    refactor: ensure add factory method

--- tests/unit/data/usecases/db_find_tasks_spec.rb
+++ tests/unit/data/usecases/db_find_tasks_spec.rb
@@ -2,43 +2,49 @@
 
 require_relative '../../../../src/data/usecases/db_find_tasks'
 require_relative '../../../../src/data/contracts/db/task_repository'
-
-class TaskRepositoryStub
-  include TaskRepository
-
-  def find
-    task1 = Task.new('Title 1', 'Description 1')
-    task1.mark_as_completed
-    task2 = Task.new('Title 2', 'Description 2')
-    [task1, task2]
-  end
-end
+require_relative '../../../../src/core/models/task'
 
 describe DbFindTasks, type: :unit do
-  it 'Should return a list of tasks' do
+  class TaskRepositoryStub
+    include TaskRepository
+
+    def find
+      task1 = Task.new('Title 1', 'Description 1')
+      task1.mark_as_completed
+      task2 = Task.new('Title 2', 'Description 2')
+      [task1, task2]
+    end
+  end
+
+  def make_sut
     task_repository = TaskRepositoryStub.new
-    usecase = described_class.new(task_repository)
-    output = usecase.execute
-    expect(output[0][:title]).to be('Title 1')
-    expect(output[0][:description]).to be('Description 1')
+    usecase = DbFindTasks.new(task_repository)
+    { usecase:, task_repository: }
+  end
+
+  it 'Should return a list of tasks' do
+    sut = make_sut
+    output = sut[:usecase].execute
+    expect(output[0][:id]).to be_a(String)
+    expect(output[0][:title]).to eq('Title 1')
+    expect(output[0][:description]).to eq('Description 1')
     expect(output[0][:completed]).to be(true)
-    expect(output[1][:title]).to be('Title 2')
-    expect(output[1][:description]).to be('Description 2')
+    expect(output[1][:id]).to be_a(String)
+    expect(output[1][:title]).to eq('Title 2')
+    expect(output[1][:description]).to eq('Description 2')
     expect(output[1][:completed]).to be(false)
   end
 
   it 'Should return empty tasks' do
-    task_repository = TaskRepositoryStub.new
-    allow(task_repository).to receive(:find).and_return([])
-    usecase = described_class.new(task_repository)
-    output = usecase.execute
+    sut = make_sut
+    allow(sut[:task_repository]).to receive(:find).and_return([])
+    output = sut[:usecase].execute
     expect(output.length).to be(0)
   end
 
   it 'Should throw if TaskRepository throws' do
-    task_repository = TaskRepositoryStub.new
-    allow(task_repository).to receive(:find).and_raise(StandardError)
-    usecase = described_class.new(task_repository)
-    expect { usecase.execute }.to raise_error(StandardError)
+    sut = make_sut
+    allow(sut[:task_repository]).to receive(:find).and_raise(StandardError)
+    expect { sut[:usecase].execute }.to raise_error(StandardError)
   end
 end
