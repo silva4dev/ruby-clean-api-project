d8099f6a410da953563a7b95cc8396c8bce8fba5
silva4dev <web4dev.lucas@outlook.com>
2024-03-10 17:43:56 -0300

    refactor: ensure return nil if data is not exists

--- src/external/db/postgresql/task/task_postgresql_repository.rb
+++ src/external/db/postgresql/task/task_postgresql_repository.rb
@@ -11,6 +11,8 @@ class TaskPostgreSQLRepository
   def find
     query = 'SELECT * FROM tasks'
     data = PostgreSQLHelper.instance.execute(query)
+    return [] if data.ntuples.zero?
+
     data.map do |row|
       Task.new(row['title'], row['description']).tap do |t|
         t.instance_variable_set(:@id, row['id'])
@@ -22,6 +24,8 @@ class TaskPostgreSQLRepository
   def add(task)
     query = 'INSERT INTO tasks (title, description, completed) VALUES ($1, $2, $3) RETURNING *'
     data = PostgreSQLHelper.instance.execute(query, [task.title, task.description, task.completed])
+    return nil if data.ntuples.zero?
+
     Task.new(data.first['title'], data.first['description']).tap do |t|
       t.instance_variable_set(:@id, data.first['id'])
       t.instance_variable_set(:@completed, data.first['completed'])
@@ -31,6 +35,8 @@ class TaskPostgreSQLRepository
   def find_by_id(id)
     query = 'SELECT * FROM tasks WHERE id = $1'
     data = PostgreSQLHelper.instance.execute(query, [id])
+    return nil if data.ntuples.zero?
+
     Task.new(data.first['title'], data.first['description']).tap do |t|
       t.instance_variable_set(:@id, data.first['id'])
       t.instance_variable_set(:@completed, data.first['completed'])
@@ -41,6 +47,8 @@ class TaskPostgreSQLRepository
     query = 'UPDATE tasks SET title = $1, description = $2, completed = $3 WHERE id = $4 RETURNING *'
     data = PostgreSQLHelper.instance.execute(query,
                                              [updated_task.title, updated_task.description, updated_task.completed, id])
+    return nil if data.ntuples.zero?
+
     Task.new(data.first['title'], data.first['description']).tap do |t|
       t.instance_variable_set(:@id, data.first['id'])
       t.instance_variable_set(:@completed, data.first['completed'])
@@ -50,6 +58,8 @@ class TaskPostgreSQLRepository
   def destroy(id)
     query = 'DELETE FROM tasks WHERE id = $1 RETURNING *'
     data = PostgreSQLHelper.instance.execute(query, [id])
+    return nil if data.ntuples.zero?
+
     Task.new(data.first['title'], data.first['description']).tap do |t|
       t.instance_variable_set(:@id, data.first['id'])
       t.instance_variable_set(:@completed, data.first['completed'])
