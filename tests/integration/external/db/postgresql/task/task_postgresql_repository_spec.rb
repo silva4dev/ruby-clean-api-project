84c274add137d1a2568647431cf98726998b4ae3
silva4dev <web4dev.lucas@outlook.com>
2024-03-09 22:03:08 -0300

    refactor: ensure add factory method helper

--- tests/integration/external/db/postgresql/task/task_postgresql_repository_spec.rb
+++ tests/integration/external/db/postgresql/task/task_postgresql_repository_spec.rb
@@ -6,6 +6,11 @@ require_relative '../../../../../../src/root/config/env'
 require_relative '../../../../../../src/core/models/task'
 
 describe TaskPostgreSQLRepository, type: :integration do
+  def make_sut
+    task_repository = TaskPostgreSQLRepository.new
+    { task_repository: }
+  end
+
   before(:all) do
     PostgreSQLHelper.instance.connect(
       dbname: Env::POSTGRESQL[:DBNAME],
@@ -25,18 +30,18 @@ describe TaskPostgreSQLRepository, type: :integration do
   end
 
   it 'Should add a task' do
-    task_repository = described_class.new
-    output = task_repository.add(Task.new('Title 1', 'Description 1'))
+    sut = make_sut
+    output = sut[:task_repository].add(Task.new('Title 1', 'Description 1'))
     expect(output.id).to be_a(String)
     expect(output.title).to eq('Title 1')
     expect(output.description).to eq('Description 1')
   end
 
   it 'Should return a list of tasks' do
-    task_repository = described_class.new
-    task_repository.add(Task.new('Title 1', 'Description 1'))
-    task_repository.add(Task.new('Title 2', 'Description 2'))
-    output = task_repository.find
+    sut = make_sut
+    sut[:task_repository].add(Task.new('Title 1', 'Description 1'))
+    sut[:task_repository].add(Task.new('Title 2', 'Description 2'))
+    output = sut[:task_repository].find
     expect(output.length).to eq(2)
     expect(output[0].id).to be_a(String)
     expect(output[0].title).to eq('Title 1')
@@ -47,28 +52,28 @@ describe TaskPostgreSQLRepository, type: :integration do
   end
 
   it 'Should return a task when filtered by id' do
-    task_repository = described_class.new
-    task = task_repository.add(Task.new('Title 1', 'Description 1'))
-    output = task_repository.find_by_id(task.id)
+    sut = make_sut
+    task = sut[:task_repository].add(Task.new('Title 1', 'Description 1'))
+    output = sut[:task_repository].find_by_id(task.id)
     expect(output.id).to be_a(String)
     expect(output.title).to eq('Title 1')
     expect(output.description).to eq('Description 1')
   end
 
   it 'Should update a task' do
-    task_repository = described_class.new
-    task = task_repository.add(Task.new('Title 1', 'Description 1'))
-    output = task_repository.update(task.id, Task.new('Title 1 Updated', 'Description 1 Updated'))
+    sut = make_sut
+    task = sut[:task_repository].add(Task.new('Title 1', 'Description 1'))
+    output = sut[:task_repository].update(task.id, Task.new('Title 1 Updated', 'Description 1 Updated'))
     expect(output.id).to be_a(String)
     expect(output.title).to eq('Title 1 Updated')
     expect(output.description).to eq('Description 1 Updated')
   end
 
   it 'Should delete a task' do
-    task_repository = described_class.new
-    task = task_repository.add(Task.new('Title 1', 'Description 1'))
-    task_repository.destroy(task.id)
-    output = task_repository.find
+    sut = make_sut
+    task = sut[:task_repository].add(Task.new('Title 1', 'Description 1'))
+    sut[:task_repository].destroy(task.id)
+    output = sut[:task_repository].find
     expect(output.length).to be(0)
   end
 end
