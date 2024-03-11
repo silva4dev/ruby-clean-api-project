30a68030af47b309381d048e2c481b46009c5778
silva4dev <web4dev.lucas@outlook.com>
2024-03-10 22:35:31 -0300

    feat: add http response middleware

--- src/root/routes/grape/tasks_routes.rb
+++ src/root/routes/grape/tasks_routes.rb
@@ -10,21 +10,13 @@ class TasksRoute < Grape::API
     get '/' do
       http_response = RouteAdapter.adapt(FindTasksControllerFactory.create).call(request)
       status http_response[:status_code]
-      return http_response[:body] if http_response[:status_code] >= 200 && http_response[:status_code] <= 299
-
-      {
-        error: http_response[:body]
-      }
+      http_response[:body]
     end
 
     get '/:id' do
       http_response = RouteAdapter.adapt(FindByIdTaskControllerFactory.create).call(request)
       status http_response[:status_code]
-      return http_response[:body] if http_response[:status_code] >= 200 && http_response[:status_code] <= 299
-
-      {
-        error: http_response[:body]
-      }
+      http_response[:body]
     end
   end
 end
