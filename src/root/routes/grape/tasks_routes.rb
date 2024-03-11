4cf52ed9c0a0f2fccadf65f17c6b72c1ebbe9845
silva4dev <web4dev.lucas@outlook.com>
2024-03-11 02:56:15 -0300

    feat: ensure add task route

--- src/root/routes/grape/tasks_routes.rb
+++ src/root/routes/grape/tasks_routes.rb
@@ -4,6 +4,7 @@ require 'grape'
 require_relative '../../adapters/route_adapter'
 require_relative '../../factories/controllers/find_tasks/find_tasks_controller_factory'
 require_relative '../../factories/controllers/find_by_id_task/find_by_id_task_controller_factory'
+require_relative '../../factories/controllers/add_task/add_task_controller_factory'
 
 class TasksRoute < Grape::API
   namespace :tasks do
@@ -15,6 +16,12 @@ class TasksRoute < Grape::API
 
     get '/:id' do
       http_response = RouteAdapter.adapt(FindByIdTaskControllerFactory.create).call(request)
+      status http_response[:status_code]
+      http_response[:body]
+    end
+
+    post '/' do
+      http_response = RouteAdapter.adapt(AddTaskControllerFactory.create).call(request)
       status http_response[:status_code]
       http_response[:body]
     end
