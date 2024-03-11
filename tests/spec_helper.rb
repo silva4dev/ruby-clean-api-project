a77d812b4993e2b287b421ed3a5b0e38b2cd4641
silva4dev <web4dev.lucas@outlook.com>
2024-03-11 01:50:51 -0300

    feat: ensure add rack test

--- tests/spec_helper.rb
+++ tests/spec_helper.rb
@@ -3,8 +3,10 @@
 require 'rack/test'
 require 'json'
 require 'simplecov'
+require_relative '../src/root/server'
 
 RSpec.configure do |config|
+  config.include Rack::Test::Methods
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -15,4 +17,8 @@ RSpec.configure do |config|
   config.pattern = 'tests/**/*_spec.rb'
   config.include Rack::Test::Methods
   SimpleCov.start if ENV['COVERAGE']
+
+  def app
+    Server
+  end
 end
