18ece0b14b55011350d77bc4cc85394aa57c6b6f
silva4dev <web4dev.lucas@outlook.com>
2024-03-11 02:54:35 -0300

    feat: add task controller

---
+++ src/http/controllers/add_task/add_task_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../../helpers/http_helper'
+require_relative '../../contracts/controller'
+require_relative '../../../core/models/task'
+
+class AddTaskController
+  include Controller
+
+  # @param find_tasks [AddTask]
+  def initialize(add_task_usecase)
+    @add_task_usecase = add_task_usecase
+  end
+
+  def handle(http_request)
+    title, description = http_request[:body].values_at(:title, :description)
+    @add_task_usecase.execute(Task.new(title, description))
+    HttpHelper.no_content
+  rescue StandardError
+    HttpHelper.server_error
+  end
+end
