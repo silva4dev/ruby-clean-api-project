0df4479b6cb30511c798c3b86f62f4dcc582eb84
silva4dev <web4dev.lucas@outlook.com>
2024-03-12 20:26:55 -0300

    feat: add destroy task route

--- src/main/routes/grape/tasks_routes.rb
+++ src/main/routes/grape/tasks_routes.rb
@@ -5,6 +5,7 @@ require_relative '../../adapters/route_adapter'
 require_relative '../../factories/controllers/find_tasks/find_tasks_controller_factory'
 require_relative '../../factories/controllers/find_by_id_task/find_by_id_task_controller_factory'
 require_relative '../../factories/controllers/add_task/add_task_controller_factory'
+require_relative '../../factories/controllers/destroy_task/destroy_task_controller_factory'
 
 class TasksRoute < Grape::API
   namespace :tasks do
@@ -22,6 +23,12 @@ class TasksRoute < Grape::API
 
     post '/' do
       http_response = RouteAdapter.adapt(AddTaskControllerFactory.create).call(request)
+      status http_response[:status_code]
+      http_response[:body]
+    end
+
+    delete '/:id' do
+      http_response = RouteAdapter.adapt(DestroyTaskControllerFactory.create).call(request)
       status http_response[:status_code]
       http_response[:body]
     end
