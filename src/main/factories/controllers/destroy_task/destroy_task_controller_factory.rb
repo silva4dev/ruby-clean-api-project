f241de489c9399072d84992e08d35b6ca69778d0
silva4dev <web4dev.lucas@outlook.com>
2024-03-12 20:25:33 -0300

    feat: add destroy controller factory

---
+++ src/main/factories/controllers/destroy_task/destroy_task_controller_factory.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative '../../../../http/controllers/destroy_task/destroy_task_controller'
+require_relative '../../../../data/usecases/db_destroy_task'
+require_relative '../../../../external/db/postgresql/task/pg_task_repository'
+
+class DestroyTaskControllerFactory
+  def self.create
+    task_repository = PgTaskRepository.new
+    db_destroy_task_usecase = DbDestroyTask.new(task_repository)
+    DestroyTaskController.new(db_destroy_task_usecase)
+  end
+end
