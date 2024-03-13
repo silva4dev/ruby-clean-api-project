de78f9b66e990d9a93b341e55c3aa4bfdf273994
silva4dev <web4dev.lucas@outlook.com>
2024-03-13 17:30:18 -0300

    refactor: ensure rename database

--- src/main/config/env.rb
+++ src/main/config/env.rb
@@ -3,7 +3,7 @@
 module Env
   PORT = ENV['PORT']&.to_i || 5050
   POSTGRESQL = {
-    DBNAME: ENV['POSTGRES_DB'] || 'grape-clean-api',
+    DBNAME: ENV['POSTGRES_DB'] || 'ruby-grape-clean-api',
     USER: ENV['POSTGRES_USER'] || 'postgres',
     PASSWORD: ENV['POSTGRES_PASSWORD'] || '123456',
     HOST: ENV['POSTGRES_HOST'] || 'localhost',
