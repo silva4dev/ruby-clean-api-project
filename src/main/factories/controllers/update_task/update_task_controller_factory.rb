2a7a1c6744ad73742dc3558239c6934b094fe1f9
silva4dev <web4dev.lucas@outlook.com>
2024-03-12 22:37:54 -0300

    feat: add update task controller factory

---
+++ src/main/factories/controllers/update_task/update_task_controller_factory.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative '../../../../http/controllers/update_task/update_task_controller'
+require_relative '../../../../data/usecases/db_update_task'
+require_relative '../../../../external/db/postgresql/task/pg_task_repository'
+
+class UpdateTaskControllerFactory
+  def self.create
+    task_repository = PgTaskRepository.new
+    db_update_task_usecase = DbUpdateTask.new(task_repository)
+    UpdateTaskController.new(db_update_task_usecase)
+  end
+end
