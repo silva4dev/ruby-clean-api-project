f4dbf70c55e03165b400627662fbb63889e9672f
silva4dev <web4dev.lucas@outlook.com>
2024-03-13 00:35:27 -0300

    refactor: ensure change port to server

--- config.ru
+++ config.ru
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# \ -p 3333 -o 0
+# \ -p 5050 -o 0
 
 require_relative 'src/main/server'
 run Server
--- src/main/config/env.rb
+++ src/main/config/env.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 module Env
-  PORT = ENV['PORT']&.to_i || 3333
+  PORT = ENV['PORT']&.to_i || 5050
   POSTGRESQL = {
-    DBNAME: ENV['DBNAME'] || 'ruby-grape-clean-api',
-    USER: ENV['USER'] || 'postgres',
-    PASSWORD: ENV['PASSWORD'] || '123456',
-    HOST: ENV['HOST'] || 'localhost',
-    PORT: ENV['PORT'] || 5432
+    DBNAME: ENV['POSTGRES_DB'] || 'grape-clean-api',
+    USER: ENV['POSTGRES_USER'] || 'postgres',
+    PASSWORD: ENV['POSTGRES_PASSWORD'] || '123456',
+    HOST: ENV['POSTGRES_HOST'] || 'localhost',
+    PORT: ENV['POSTGRES_PORT'] || 5432
   }.freeze
 end
