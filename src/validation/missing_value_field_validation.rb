006a7520a3cf5524eeaf46d4f45b8e25b2896d8d
silva4dev <web4dev.lucas@outlook.com>
2024-03-16 15:56:29 -0300

    feat: add missing value field validation

---
+++ src/validation/missing_value_field_validation.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative '../../src/presentation/errors/missing_value_error'
+require_relative '../../src/presentation/contracts/validation'
+
+class MissingValueFieldValidation
+  include Validation
+
+  attr_reader :field_name
+
+  def initialize(field_name)
+    @field_name = field_name
+  end
+
+  def validate(input)
+    return MissingParamError.new(@field_name) if input.empty?
+
+    MissingValueError.new(@field_name) if input[@field_name] == ''
+  end
+end
