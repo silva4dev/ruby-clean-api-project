0ad9519e2631c13f667a99098d3f824c1aca0756
silva4dev <web4dev.lucas@outlook.com>
2024-03-16 15:56:42 -0300

    feat: add required field validation

--- src/validation/required_field_validation.rb
+++ src/validation/required_field_validation.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../../src/presentation/errors/missing_param_error'
+require_relative '../../src/presentation/errors/missing_value_error'
 require_relative '../../src/presentation/contracts/validation'
 
 class RequiredFieldValidation
@@ -13,6 +14,8 @@ class RequiredFieldValidation
   end
 
   def validate(input)
+    return MissingParamError.new(@field_name) if input.empty?
+    return MissingValueError.new(@field_name) if input[@field_name] == ''
     return if input[@field_name]
 
     MissingParamError.new(@field_name)
