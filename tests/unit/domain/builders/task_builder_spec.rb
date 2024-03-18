f7fc056b60bec4ba4ab04f2c134be71051c676a2
silva4dev <web4dev.lucas@outlook.com>
2024-03-18 19:49:59 -0300

    test: ensure add task builder

---
+++ tests/unit/domain/builders/task_builder_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative '../../../../src/domain/builders/task_builder'
+
+describe TaskBuilder, type: :unit do
+  let(:task_builder) { described_class.new }
+
+  it 'Should build a task' do
+    task = task_builder
+        .with_title('New title')
+        .with_description('New description')
+        .with_completed(true)
+        .build
+    expect(task).to be_an_instance_of(Task)
+    expect(task.title).to eq('New title')
+    expect(task.description).to eq('New description')
+    expect(task.completed).to be(true)
+  end
+end
