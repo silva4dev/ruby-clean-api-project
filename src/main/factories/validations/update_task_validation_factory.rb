6d7feee3d0a2db54bd89b313874d74c8d216c9b1
silva4dev <web4dev.lucas@outlook.com>
2024-03-18 02:14:31 -0300

    feat: add type field validation

--- src/main/factories/validations/update_task_validation_factory.rb
+++ src/main/factories/validations/update_task_validation_factory.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 require_relative '../../../validation/missing_value_field_validation'
+require_relative '../../../validation/type_field_validation'
 require_relative '../../../validation/validation_composite'
 
 class UpdateTaskValidationFactory
   def self.create
     validations = []
-    %i[title description].each do |field|
+    %i[title description completed].each do |field|
       validations.push(MissingValueFieldValidation.new(field))
+      validations.push(TypeFieldValidation.new(field, String)) if field == :title
+      validations.push(TypeFieldValidation.new(field, String)) if field == :description
     end
     ValidationComposite.new(validations)
   end
