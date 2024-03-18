def165b8150ff2b432a63f4f54700d81ecf2b7b8
silva4dev <web4dev.lucas@outlook.com>
2024-03-18 02:14:02 -0300

    test: ensure rename class

--- tests/unit/domain/models/task_spec.rb
+++ tests/unit/domain/models/task_spec.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 require_relative '../../../../src/domain/models/task'
+require_relative '../../../../src/domain/errors/invalid_type_error'
+require_relative '../../../../src/domain/errors/missing_param_error'
 
 describe Task, type: :unit do
   it 'Should throw error when title is empty' do
-    expect { described_class.new('', 'Description') }.to raise_error(MissingValueError)
+    expect { described_class.new('', 'Description') }.to raise_error(MissingParamError)
   end
 
   it 'Should throw error when description is empty' do
-    expect { described_class.new('Title', '') }.to raise_error(MissingValueError)
+    expect { described_class.new('Title', '') }.to raise_error(MissingParamError)
   end
 
   it 'Should throw error when title is not a string' do
